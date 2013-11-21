14d21cffe1c03db40a586100261a12d31b8c2d92
Jeff Casimir <jeff@casimircreative.com>
2013-11-21 15:10:48 -0700

    Implemented scoring and word picking

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,5 +1,41 @@
 class Scrabble
   def self.score(word)
-    0
+    letters = word.to_s.upcase.split('')
+    letters.inject(0) do |accumulator, letter|
+      accumulator += score_letter(letter)
+    end
+  end
+
+  def self.score_letter(letter)
+    letter_scores[letter]
+  end
+
+  def self.highest_score_from(list)
+    score, top_words = grouped_by_score(list).max
+    bonus_word_from(top_words) || shortest_from(top_words)
+  end
+
+  def self.bonus_word_from(words)
+    words.detect{|word| word.length == 7}
+  end
+
+  def self.shortest_from(words)
+    words.min_by{|word| word.length}
+  end
+
+  def self.grouped_by_score(list)
+    list.group_by{|word| score(word)}
+  end
+
+  def self.letter_scores
+    {
+      "A"=>1, "B"=>3, "C"=>3, "D"=>2,
+      "E"=>1, "F"=>4, "G"=>2, "H"=>4,
+      "I"=>1, "J"=>8, "K"=>5, "L"=>1,
+      "M"=>3, "N"=>1, "O"=>1, "P"=>3,
+      "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
+      "U"=>1, "V"=>4, "W"=>4, "X"=>8,
+      "Y"=>4, "Z"=>10
+    }
   end
 end
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -8,4 +8,40 @@ class ScrabbleTest < MiniTest::Unit::TestCase
     assert_equal 1, Scrabble.score("a")
     assert_equal 4, Scrabble.score("f")
   end
+
+  def test_it_scores_a_word
+    assert_equal 8, Scrabble.score("hello")
+  end
+
+  def test_it_scores_nil_as_zero
+    assert_equal 0, Scrabble.score(nil)
+  end
+
+  def test_it_scores_empty_string_as_zero
+    assert_equal 0, Scrabble.score("")
+  end
+
+  def test_it_picks_the_higest_scoring_word_from_a_list_of_one
+    assert_equal "home", Scrabble.highest_score_from(['home'])
+  end
+
+  def test_it_picks_the_higest_scoring_word_from_a_list_of_two
+    assert_equal "home", Scrabble.highest_score_from(['home', 'word'])
+  end
+
+  def test_it_picks_the_higest_scoring_word_from_a_list_of_four
+    assert_equal "home", Scrabble.highest_score_from(['home', 'word', 'hello', 'sound'])
+  end
+
+  def test_it_picks_the_shortest_high_scoring_word_from_a_list
+    assert_equal "word", Scrabble.highest_score_from(['hello', 'word', 'sound'])
+  end
+
+  def test_it_picks_the_bonus_word_from_the_highscoring_set
+    assert_equal "silence", Scrabble.highest_score_from(['home', 'word', 'silence'])
+  end
+
+  def test_it_picks_the_first_word_when_theres_no_more_clear_choice
+    assert_equal "word", Scrabble.highest_score_from(['hi', 'word', 'ward'])
+  end
 end
